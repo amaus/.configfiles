9c5acef3ddfa0bb3e17a7d835536229e7f814f70
Aaron Maus <arobie2@gmail.com>
2020-08-03 18:09:52 -0500

    Fix mistake in setup.zsh
    correctly symlink .vim dir

--- setup.zsh
+++ setup.zsh
@@ -1,7 +1,7 @@
 ln -s -i ~/.configfiles/.gitconfig ~/.gitconfig
 ln -s -i ~/.configfiles/.zshrc ~/.zshrc
 ln -s -i ~/.configfiles/.vimrc ~/.vimrc
-ln -s -i ~/.configfiles/.vim ~
+ln -s -i ~/.configfiles/.vim ~/.vim
 mkdir ~/.configfiles/.vim/colors
 cp ~/.configfiles/solarized/vim-colors-solarized/colors/solarized.vim ~/.configfiles/.vim/colors/
 ln -s -i ~/.configfiles/.tmux.conf ~/.tmux.conf
